5565d6e38514b594b8d1a008f70f05097c0287af
Anton Böhler <antboehler@gmail.com>
2024-08-17 15:33:51 +0200

    improvements

--- scripts/card-list.js
+++ scripts/card-list.js
@@ -4,7 +4,7 @@
  */
 function openCardList(cards) {
     const cardListContainer = document.getElementById("card-list").nextElementSibling;
-    cardListContainer.children[0].setAttribute("data-folder", cards[0].folder);
+    cardListContainer.children[0].children[1].innerText = cards[0].folder;
     const cardList = cardListContainer.children[1];
 
     const cardElements = cards.map(card => {
--- scripts/card-swipe.js
+++ scripts/card-swipe.js
@@ -1,40 +1,42 @@
 let swipeCards;
-let cardSwipe;
+let swipeFolderElement;
 let swipeCardElement1;
 let swipeCardElement2;
 let nextSwipeCard;
-let completedCards;
+let completedCardsCount;
 let progressElement;
 
 function initializeSwipeCard() {
-    cardSwipe = document.getElementById("card-swipe").nextElementSibling.children[1];
-    progressElement = cardSwipe.nextElementSibling;
+    let container = document.getElementById("card-swipe").nextElementSibling;
+    swipeFolderElement = container.children[0].children[1];
+    let cardContainer = container.children[1];
+    progressElement = cardContainer.nextElementSibling;
     swipeCardElement1 = createCard();
     swipeCardElement2 = createCard();
-    cardSwipe.replaceChildren(
+    cardContainer.replaceChildren(
         swipeCardElement1,
         swipeCardElement2
     )
 
-    cardSwipe.addEventListener("touchstart", () => {
+    cardContainer.addEventListener("touchstart", () => {
         motionCompleted = false;
     }, { passive: true });
-    cardSwipe.addEventListener("touchend", () => {
+    cardContainer.addEventListener("touchend", () => {
         motionCompleted = true;
     }, { passive: true });
 
-    cardSwipe.addEventListener("scroll", (e) => {
-        if (cardSwipe.scrollLeft > window.innerWidth / 2 && motionCompleted) {
+    cardContainer.addEventListener("scroll", (e) => {
+        if (cardContainer.scrollLeft > window.innerWidth / 2 && motionCompleted) {
             // hijack scroll
-            cardSwipe.style.overflowX = "hidden";
-            cardSwipe.scrollLeft = 0;
+            cardContainer.style.overflowX = "hidden";
+            cardContainer.scrollLeft = 0;
             motionCompleted = false;
 
             toNextSwipeCard();
 
             setTimeout(() => {
                 // give back
-                cardSwipe.style.overflowX = "scroll";
+                cardContainer.style.overflowX = "scroll";
             }, 0);
         }
     }, { passive: true });
@@ -45,12 +47,11 @@ function initializeSwipeCard() {
  * @param {[Card]} cards
  */
 function openCardSwipe(cards) {
-    completedCards = 0;
-    progressElement.style.setProperty("--completed-count", completedCards);
+    completedCardsCount = 0;
+    progressElement.style.setProperty("--completed-count", completedCardsCount);
     swipeCards = cards;
 
-    const cardSwipeContainer = document.getElementById("card-swipe").nextElementSibling;
-    cardSwipeContainer.children[0].setAttribute("data-folder", swipeCards[0].folder);
+    swipeFolderElement.innerText = cards[0].folder;
 
     let currentSwipeCard = swipeCards[Math.floor(Math.random() * swipeCards.length)];
     nextSwipeCard = swipeCards[Math.floor(Math.random() * swipeCards.length)];
@@ -63,8 +64,8 @@ function toNextSwipeCard() {
     fillCard(swipeCardElement1, nextSwipeCard);
 
     nextSwipeCard = swipeCards[Math.floor(Math.random() * swipeCards.length)];
-    completedCards++;
-    progressElement.style.setProperty("--completed-count", completedCards);
+    completedCardsCount++;
+    progressElement.style.setProperty("--completed-count", completedCardsCount);
 
     fillCard(swipeCardElement2, nextSwipeCard);
 }
